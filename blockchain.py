cf23d724b23c605e9a298e71c471a1ffae72c0fe
Abdullah Feroz <00maferoz@gmail.com>
2025-04-14 23:17:43 +0500

    Created a class Blockchain that will manage the complete chain of the blocks

--- blockchain.py
+++ blockchain.py
@@ -13,3 +13,23 @@ class Block:
         # Concatenate block contents and calculate the hash
         block_string = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
         return hashlib.sha256(block_string.encode('utf-8')).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        # Create the first block (genesis block)
+        genesis_block = Block(0, "Genesis Block", "0")
+        self.chain.append(genesis_block)
+
+    def add_block(self, data):
+        # Add a new block to the chain
+        last_block = self.chain[-1]
+        new_block = Block(last_block.index + 1, data, last_block.hash)
+        self.chain.append(new_block)
+
+    def get_chain(self):
+        # Return the entire blockchain
+        return self.chain
